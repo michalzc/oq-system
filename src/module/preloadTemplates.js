dfc4fd2a607dc09cc59c3b10aab69cf21e42486e
Michał Zając <michal.t.zajac@gmail.com>
2024-01-12 17:17:05 +0100

    Adding Assets

--- src/module/preloadTemplates.js
+++ src/module/preloadTemplates.js
@@ -1,11 +1,5 @@
-// SPDX-FileCopyrightText: 2022 Johannes Loher
-//
-// SPDX-License-Identifier: MIT
-
 export async function preloadTemplates() {
-  const templatePaths = [
-    // Add paths to "systems/fvtt-system-oq/templates"
-  ];
+  const templatePaths = [];
 
   return loadTemplates(templatePaths);
 }
