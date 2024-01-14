813f56a9b3128f5d622682a0073b469a5783b883
Michał Zając <michal.t.zajac@gmail.com>
2024-01-14 20:25:38 +0100

    [OQFVTT-1] Just one method stub for creating PR

--- src/module/document/item/baseItem.js
+++ src/module/document/item/baseItem.js
@@ -2,4 +2,8 @@ export class OQBaseItem extends Item {
   async _preCreate(data, option, user) {
     super._preCreate(data, option, user);
   }
+
+  roll() {
+    //TODO: Roll implementation for this element.
+  }
 }
